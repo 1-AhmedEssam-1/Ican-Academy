e35d23276952ab543325d8f5e8fe57307fc9b46c
ahmed essam <ahmed.essam202062@gmail.com>
2023-07-01 19:25:47 +0300

    ---

--- generatingSessions.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto');
-
-function generateSessionSecret() {
-  const sessionSecretBytes = crypto.randomBytes(32);
-  const sessionSecret = sessionSecretBytes.toString('hex');
-  return sessionSecret;
-}
-
-module.exports = generateSessionSecret;
